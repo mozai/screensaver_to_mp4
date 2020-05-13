8998a69873d3dbdad819c46a8fec997477ddc344
Moses Moore <mozai@users.noreply.github.com>
2020-05-12 22:52:32 -0400

    improvements

--- record_xscreensaver.sh
+++ record_xscreensaver.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-#   inspired by a script by Albert Veli
+#!/usr/bin/env bash
+# fires up an xscreensaver hack and records it to an .mp4 video
+# thx to Albert Veli for a hint and Jamie Zawinski for XScreenSaver
 set -eu
 
 ##########
@@ -11,12 +12,14 @@ fps=${fps:-30}  # decent values are 24,25,30,60
 ##########
 # init
 die() { >&2 echo "$*"; exit 1; }
-quack() { [ "${debug}" ] && echo "$*"; }
+quack() { if [ "${debug}" ];then echo "$*"; fi; }
 
 command -v xwininfo >/dev/null || die "install xwininfo first"
 command -v ffmpeg >/dev/null || die "install ffmpeg first"
-ffmpeg -formats 2>/dev/null | grep -iq x11grab || die "Fatal: ffmpeg does not support x11 grabbing, try compiling it yourself with appropriate flags"
-ffmpeg -codecs 2>/dev/null | grep -iq libx264 || die "Fatal: ffmpeg does not support libx264, try compiling it yourself with appropriate flags" # or edit the scipt and choose another codec
+ffmpeg -formats 2>/dev/null | grep -q '^ D. x11grab ' \
+  || die "Fatal: ffmpeg cannot do x11grab; try another version or compile your own?"
+ffmpeg -codecs 2>/dev/null | grep -q '^ .EV... h264 ' \
+  || die "Fatal: ffmpeg cannot do libx264; I really want to use libx264"
 
 ##########
 # main
@@ -35,7 +38,7 @@ outfile="$(basename "${hackname}").mp4"
 
 "$hackname" "$@" &
 cpid=$!
-txt=$(xwininfo -id "$(xdotool search --sync --all --onlyvisible --limit 1  --name "from the XScreenSaver" )")
+txt=$(xwininfo -id "$(xdotool search --sync --all --onlyvisible --limit 1 --name "from the XScreenSaver" )")
 [ "$txt" ] || die "Error getting window information."
 x=$(<<<"$txt" awk '/^ *Absolute upper-left X:/{print $4}')
 y=$(<<<"$txt" awk '/^ *Absolute upper-left Y:/{print $4}')
@@ -49,13 +52,12 @@ tempfile=$(mktemp ./tmp.XXXXXXXX.mkv)
 ffmpeg -hide_banner -v warning -threads 0 \
   -f x11grab -r "${fps}" -t "${duration}" \
   -s "${w}x${h}" -i ":0.0+${x},${y}" \
-  -c:v libx264 -preset ultrafast -qp 0 \
+  -c:v h264 -preset ultrafast -qp 0 \
   -an -y "${tempfile}"
 kill "$cpid"
 echo ""
 quack "$(ls -lh "${tempfile}")"
-ffmpeg -hide_banner -v warning -threads 0 \
-  -i "${tempfile}" -c:v h264 -preset slow -an "${outfile}" \
+ffmpeg -hide_banner -v warning -threads 0 -i "${tempfile}" \
+  -an -c:v h264 -pix_fmt yuv420p -preset slow "${outfile}" \
   && rm "${tempfile}"
 ls -lh "${outfile}"
-
